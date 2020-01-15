535ef17f18c8f0400469606064efdf175e8ebb78
NSACEP <57938158+NSACEP@users.noreply.github.com>
2020-01-15 16:12:40 +0700

    Add files via upload

---
+++ after-school-computer-science-03/bruteForce.js
@@ -0,0 +1,13 @@
+
+var passwords = [];
+
+function combos(){
+  for(){
+
+  }
+}
+
+document.querySelector("#usr").value = "NPS17"
+for(var i = 0; i < passwords.length){
+  document.querySelector("#pass").value = passwords[i];
+}
